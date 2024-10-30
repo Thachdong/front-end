dcd61ccae2c6d7dbdc84fdae21c74966716b345a
Thachdong <thachdong270293@gmail.com>
2024-10-30 19:46:36 +0700

    feat(react-webpack): setup project from scratch

---
+++ packages/react-webpack/src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client"
+import { App } from "./app"
+
+const domNode = document.getElementById('root')
+
+if (!domNode) {
+    throw new Error("Dom container not found!")
+}
+
+const root = createRoot(domNode)
+
+root.render(<App />)
